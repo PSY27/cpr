22af42ca718d83e5af3639c8dbf5e01016753dba
SAI SRAVAN MEDICHERLA <saisraone2002@gmail.com>
2022-07-05 23:12:54 +0530

    Revert "Delete codes/STL directory"
    This reverts commit d66bde67daaa8f38c5f1d058b64cb3d1b784f39d.

---
+++ codes/STL/arrayofvectors.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printVec(vector <int> v)// will use &v if we dont want to make a copy of the vector as its expensive o(n) so we pass by refernce to affectits changes
+{
+    cout<<"size"<<v.size()<<endl;
+    for (int i = 0; i < v.size(); i++)
+                    //v.size()->o(1)
+    {
+        /* code */
+        cout<<v[i]<<" ";
+
+    }
+    cout<<endl;
+
+}
+//
+int main(){
+    int N;
+    cin>>N;
+    vector<vector<int>> v;
+    for (int i = 0; i < N; i++)
+    {
+        /* code */
+        int n;
+        cin>>n;
+        for(int j = 0 ; j<n ;j++){
+            int  x;
+            cin>>x;
+            v[i].push_back(x);
+        }
+        
+    }
+    for (int i = 0; i < N; i++)
+        {
+            /* code */
+            printVec(v[i]);
+        }
+    
+    return 0;
+}
